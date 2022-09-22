09a5ddeedaef7712b27d53111fb98ee4a197740b
Lena <len.dal@yandex.ru>
2022-09-22 14:57:14 +0300

    2task

--- 2task.py
+++ 2task.py
@@ -7,28 +7,6 @@
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота ""интеллектом""
 
-# import random
-
-
-# player1=str(input("Введите имя первого игрока: "))
-# player2=str(input("Введите имя второго игрока: "))
-# total_candy=int(input("Сколько всего конфет? ")) 
-
-# # how_many_candy=int(input("Возьмите от 1 до 3 конфет: "))
-# def random_player():
-#     i=random.randint(0,1)    
-#     if i == 1:
-#         print(f"Первый ход у игрока ", player1)
-#     else:
-#         print(f"Первый ход у игрока ", player2)
-#         player1,player2=player2,player1        
-# random_player()
-
-# while total_candy>=3:
-#     print("Конфеты тянет ", player1)
-#     player1_take=int(input("Сколько возьмешь конфет (от 1 до 3): "))
-#     if player1_take > 3 or player1_take <1:
-#         print("Только от 1 до 3!")
 
 import random
 
